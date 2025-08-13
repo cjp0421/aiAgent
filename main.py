4078ce869d05da59d7957dfe2a4de6cbf5d6020f
cjp0421 <76797183+cjp0421@users.noreply.github.com>
2025-08-13 18:35:50 -0500

    Added support for verbose cli flag and cleaned up response printed to terminal when user submits prompt without flag

--- main.py
+++ main.py
@@ -5,13 +5,13 @@ from google import genai
 from google.genai import types
 
 def main():
-    print("Hello from aiagent!")
+    print("Hello from aiAgent!")
     load_dotenv()
     if len(sys.argv) < 2:
         sys.exit("error: no prompt provided")
     api_key = os.environ.get("GEMINI_API_KEY")
     messages = [
-    types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    types.Content(role="user", parts=[types.Part(text=sys.argv[1])]),
 ]
     client = genai.Client(api_key=api_key)
     response = client.models.generate_content(
@@ -19,9 +19,14 @@ def main():
         contents=f"{messages}"
     )
 
-    print(response.text)
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    if len(sys.argv) > 2 and sys.argv[2] == '--verbose':
+        print(f"User prompt: {sys.argv[1]}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+        print(f"{response.text}")
+
+    else:
+        print(f"{response.text}")
 
 if __name__ == "__main__":
     main()
