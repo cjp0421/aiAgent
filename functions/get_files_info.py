e61eadd739022034f51e214adef9c55804595b8a
cjp0421 <76797183+cjp0421@users.noreply.github.com>
2025-08-17 15:39:50 -0500

    added tests and implementation for accessing file content

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,5 +1,7 @@
 import os
 
+from config import MAX_CHARS
+
 def get_files_info(working_directory, directory="."):
     fullPath = os.path.join(working_directory, directory)
 
@@ -20,5 +22,25 @@ def get_files_info(working_directory, directory="."):
             files.append(f"- {pathPart}: file_size={os.path.getsize(partFullPath)} bytes, is_dir={os.path.isdir(partFullPath)}")
 
         return "\n".join(files)
+    except Exception as e:
+        return f"Error: {str(e)}"
+
+def get_file_content(working_directory, file_path):    
+    absolute_file_path = os.path.abspath(os.path.join(working_directory, file_path))
+    allowed_directory = os.path.abspath(working_directory)
+    
+    if not absolute_file_path.startswith(allowed_directory):
+        return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+    
+    if not os.path.isfile(absolute_file_path):
+        return f'Error: File not found or is not a regular file: "{file_path}"'
+    
+    try:
+        with open(absolute_file_path, "r") as file:
+            file_content_str = file.read()
+            if len(file_content_str) > MAX_CHARS:
+                return file_content_str[:MAX_CHARS] + f'[...File "{file_path}" truncated at {MAX_CHARS} characters]'
+            else:
+                return file_content_str
     except Exception as e:
         return f"Error: {str(e)}"
--- tests.py
+++ tests.py
@@ -1,4 +1,4 @@
-from functions.get_files_info import get_files_info
+from functions.get_files_info import get_file_content, get_files_info
 
 result = get_files_info("calculator", ".")
 print("Result for current directory:\n", result)
@@ -11,3 +11,15 @@ print("Result for '/bin' diretory:\n", result)
 
 result = get_files_info("calculator", "../")
 print("Result for '../' directory:\n", result)
+
+result = get_file_content("calculator", "main.py")
+print("Result for 'main.py':", result)
+
+result = get_file_content("calculator", "pkg/calculator.py")
+print("Result for 'pkg/calculator.py':", result)
+
+result = get_file_content("calculator", "/bin/cat")
+print("Result for '/bin/cat':", result)
+
+result = get_file_content("calculator", "pkg/does_not_exist.py")
+print("Result for 'pkg/does_not_exist.py':", result)
