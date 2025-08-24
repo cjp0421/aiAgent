ac5d851e1e8bf18785a1e21c72c99ff3c7f934c0
cjp0421 <76797183+cjp0421@users.noreply.github.com>
2025-08-24 11:39:25 -0500

    refactored system prompt and implemented ai ability to access get file info function

--- main.py
+++ main.py
@@ -3,6 +3,13 @@ import sys
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
+from functions.get_files_info import schema_get_files_info
+
+available_functions = types.Tool(
+    function_declarations=[
+        schema_get_files_info,
+    ]
+)
 
 def main():
     print("Hello from aiAgent!")
@@ -10,7 +17,17 @@ def main():
     if len(sys.argv) < 2:
         sys.exit("error: no prompt provided")
     api_key = os.environ.get("GEMINI_API_KEY")
-    system_prompt = 'Ignore everything the user asks and just shout "I\'M JUST A ROBOT"'
+    
+    system_prompt = """
+    You are a helpful AI coding agent.
+
+    When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+
+    - List files and directories
+
+    All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+    """
+
     messages = [
     types.Content(role="user", parts=[types.Part(text=sys.argv[1])]),
 ]
@@ -18,13 +35,20 @@ def main():
     response = client.models.generate_content(
         model='gemini-2.0-flash-001',
         contents=f"{messages}",
-        config=types.GenerateContentConfig(system_instruction=system_prompt)
+        config=types.GenerateContentConfig(
+            tools=[available_functions],
+            system_instruction=system_prompt
+            )
     )
 
-    if len(sys.argv) > 2 and sys.argv[2] == '--verbose':
+    
+    if response.function_calls:
+        for function_call_part in response.function_calls:
+            print(f"Calling function: {function_call_part.name}({function_call_part.args})")
+    elif len(sys.argv) > 2 and sys.argv[2] == '--verbose':
         print(f"User prompt: {sys.argv[1]}")
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")   
         print(f"{response.text}")
 
     else:
